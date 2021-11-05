6d0187c36e722ba610f20b2a2203ad3f6821e5b5
Anton <anton27081986@gmail.com>
2021-11-05 07:42:46 +0300

    add search & filter

--- my-app/src/components/app/app.js
+++ my-app/src/components/app/app.js
@@ -12,10 +12,12 @@ class App extends Component {
         super(props);
         this.state = {
             data: [
-                {name: 'Anton Antonov' , salary: '1000' , increase: false, id: 1 },
-                {name: 'Ivan Ivanov' , salary: '2000', increase: true, id: 2},
-                {name: 'Petr Petrov' , salary: '3000', increase: false, id: 3 },
-            ]
+                {name: 'Anton Antonov' , salary: '1000' , increase: false, rise:true, id: 1 },
+                {name: 'Ivan Ivanov' , salary: '2000', increase: true, rise:false, id: 2},
+                {name: 'Petr Petrov' , salary: '3000', increase: false, rise:false, id: 3 },
+            ],
+            term: '',
+            filter: 'all'
         }
         this.maxId = 4;
     }
@@ -33,7 +35,14 @@ class App extends Component {
     }
 
     addItem = (name, salary) => {
-        const obj = {name, salary, increase: false, id: this.maxId++}
+        const obj = {
+            name, 
+            salary, 
+            increase: false, 
+            rise: false,
+            id: this.maxId++
+        }
+
         this.setState(({data}) => {
             // const newArr = [...data, obj]
             return {
@@ -41,19 +50,93 @@ class App extends Component {
             }
         })
     }
+
+    onToggleProp = (id, prop) => {
+        // this.setState(({data}) => {
+        //     const index = data.findIndex(elem => elem.id === id);
+
+        //     const old = data[index]; 
+        //     const newItem = {...old, increase: !old.increase};
+        //     const newArr = [...data.slice(0, index), newItem, ...data.slice(index + 1)]
+        
+        //     return {
+        //         data: newArr
+        //     }
+            
+        // })
+        this.setState(({data}) => ({
+            data: data.map(item => {
+                if (item.id === id) {
+                    return {...item, [prop]: !item[prop]}
+                }
+                return item;
+            })
+        }))
+    }
+
+    // onToggleRise = (id) => {
+    //     this.setState(({data}) => ({
+    //         data: data.map(item => {
+    //             if (item.id === id) {
+    //                 return {...item, rise: !item.rise}
+    //             }
+    //             return item;
+    //         })
+    //     }))
+    // }
+
+    searchEmp = (items, term) => {
+        if (term.length === 0) {
+            return items;
+        }
+
+        return items.filter(item => {
+            return item.name.indexOf(term) > -1;
+        })
+    }
+
+    onUpdateSearch = (term) => {
+        this.setState({
+            term: term,
+        })
+    }
+
+    filterPost = (items, filter) => {
+        switch (filter) {
+            case 'rise':
+                return items.filter(item => item.rise);
+            case 'moreThen1000':
+                return items.filter(item => item.salary > 1000);
+            default:
+                return items;
+        }
+    }
+
+    onFilterSelect = (filter) => {
+        this.setState({
+            filter: filter
+        })
+    }
      
     render() {
+        const {data, term, filter} = this.state;
+        const employees = this.state.data.length;
+        const increased = this.state.data.filter(item => item.increase).length;
+        const vivsibleData = this.filterPost(this.searchEmp(data, term), filter);
+        
         return (
             <div className='app'>
-                <AppInfo/>
+                <AppInfo employees={employees} increased={increased}/>
                 
                 <div className="search-panel">
-                    <SearchPanel/>
-                    <AppFilter/>
+                    <SearchPanel onUpdateSearch={this.onUpdateSearch}/>
+                    <AppFilter filter={filter} onFilterSelect={this.onFilterSelect}/>
                 </div>
                 <Employerslist 
-                    data={this.state.data}
-                    onDelete={this.deleteItem}/>
+                    data={vivsibleData}
+                    onDelete={this.deleteItem}
+                    onToggleProp={this.onToggleProp}
+                    />
                 <EmployersAddForm
                     onAdd={this.addItem}
                     />
